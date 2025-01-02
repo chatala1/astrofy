09d716c96eb9d5f3316461b43c69ebe3d5e5c95b
chatala1 <16328550+chatala1@users.noreply.github.com>
2025-01-02 01:13:12 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
-import { defineConfig } from 'astro/config'; import mdx from '@astrojs/mdx'; import sitemap from '@astrojs/sitemap'; import tailwind from '@astrojs/tailwind'; import netlify from '@astrojs/netlify'; export default defineConfig({ site: 'https://cosmosis.netlify.app', output: 'hybrid', // or 'server' integrations: [mdx(), sitemap(), tailwind(), netlify()] });
-export async function getStaticPaths() {
-  return {
-    paths: [],
-    fallback: 'blocking', // or true
-  };
-}
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify';
 
+export default defineConfig({
+  integrations: [mdx(), sitemap(), tailwind(), netlify()],
+  site: 'https://chadhatala.netlify.app'
+});
