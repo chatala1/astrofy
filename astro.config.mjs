ab2fa92e4245696c4e1e5bdac309113d2c73c228
chatala1 <16328550+chatala1@users.noreply.github.com>
2025-01-02 22:36:20 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-import netlify from '@astrojs/netlify';
+import netlify from '@astrojs/netlify/functions';
 
 export default defineConfig({
-  integrations: [mdx(), sitemap(), tailwind(), netlify()],
-  site: 'https://chadhatala.netlify.app'
+  site: 'https://chadhatala.netlify.app',
+  output: 'hybrid'
+  integrations: [mdx(), sitemap(), tailwind(), netlify()]
 });
