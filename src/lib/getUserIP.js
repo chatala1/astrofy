f6b8258d1810415d62efcef08423f083c5f926b8
chatala1 <16328550+chatala1@users.noreply.github.com>
2025-01-02 23:06:22 -0500

    Delete src/lib/getUserIP.js

--- src/lib/getUserIP.js
+++
@@ -1,6 +0,0 @@
-export async function getUserIP(req) {
-  const headers = req.headers || {};
-  const forwarded = headers['x-forwarded-for'];
-  const ip = forwarded ? forwarded.split(/, /)[0] : req.connection?.remoteAddress;
-  return ip;
-}
